bd5dd6c29fd4ab79e05004928aa5a93c1af8ac9d
Satyam Mishra <satyammishra9050@gmail.com>
2023-12-31 16:14:35 +0530

    vercel deployment fix-2 (const style variable use in className)

--- src/app/style.js
+++ src/app/style.js
@@ -1,7 +1,7 @@
 const styles = {
-    paddingX: "sm:px-16 px-6",
-    paddingY: "sm:py-16 py-6",
-    padding: "sm:px-16 sm:py-16 p-8 pt-40 md:p-40",
+    paddingX: `sm:px-16 px-6`,
+    paddingY: `sm:py-16 py-6`,
+    padding: `sm:px-16 sm:py-16 p-8 pt-40 md:p-40`,
 
 
 };
