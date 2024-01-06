7a192ac6aa782fcb8bede986f324cfb90e561495
Satyam Mishra <satyammishra9050@gmail.com>
2024-01-06 20:55:28 +0530

    added google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 // These styles apply to every route in the application
+import Script from 'next/script'
 import './global.css'
 
 export const metadata = {
@@ -9,6 +10,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-2Z9QN49SME"/>
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-2Z9QN49SME');
+        `}
+        </Script>
       <body>{children}</body>
     </html>
   )
